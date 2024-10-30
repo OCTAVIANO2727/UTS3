f8049c0bb882a03786afe16c96844082f966b796
OCTAVIANO2727 <octabayuramadhani@gmail.com>
2024-10-30 20:58:28 +0700

    uploading
    upload

---
+++ img/style.cs
@@ -0,0 +1,35 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #333;
+    color: Yellow;
+    padding: 10px 0;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+nav ul li a {
+    color: Black;
+    text-decoration: none;
+}
+
+.produk {
+    padding: 20px;
+}
+
+.item {
+    display: inline-block;
+    margin-right: 20px;
+}
